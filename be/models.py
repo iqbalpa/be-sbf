adfcc4ca35b7615bc9f74763838aa25bf3dd0843
Falensi Azmi <falensiazmi@gmail.com>
2021-12-07 22:12:57 +0700

    menambahkan models

--- be/models.py
+++ be/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Film(models.Model):
+    title = models.CharField(max_length=50)
+    poster = models.ImageField()
+    trailer = models.CharField(max_length=200)
+    genre = models.CharField(max_length=20)
+    year_released = models.IntegerField()
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
